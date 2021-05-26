0b565ed815d9814f06225fcb9bd551e240afe64e
Darion Thompson <darionthompson2@gmail.com>
2021-05-26 19:31:50 -0400

    initial commit

---
+++ GitRefresh/GitRefresh/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GitRefresh
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
